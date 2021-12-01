1ef95e0841e15e2d2d7ddd9e7514da7ee195edd3
elxan-code <92145329+elxan-code@users.noreply.github.com>
2021-12-02 02:57:56 +0400

    Add files via upload

---
+++ javascript/home.js
@@ -0,0 +1,50 @@
+//#region task 1
+// alert("sum of number");
+// let num1 = Number(prompt("Enter the number 1:"));
+//  let num2 = Number(prompt("Enter the number 2:"));
+//  alert(num1 + num2);
+// #endregion
+
+//#region task 4
+// alert("reverse number");
+// let a = prompt("Enter the number");
+// let b, sum = 0;
+// let x = a;
+// while(a > 0)
+// {
+//   b = a % 10;
+//   sum = sum * 10 + b;
+//   a = parseInt(a / 10);
+// }
+// alert(sum);
+//#endregion
+
+//#region task 5
+// alert("duzbucaqlinin sahesini hesablmaq");
+// let a = prompt("Duzbucaqlinin eni");
+// let b = prompt("Duzbucaqlinin Sahesi");
+// alert(a*b);
+//#endregion
+
+//#region  task 2
+// let a = 5;
+// let b = "";
+// for (let i = 1; i <= a; i++) {
+//   for (let j = 0; j < i; j++) {
+//     b += "*";
+//   }
+//   b += "\n";
+// }
+// alert(b);
+//#endregion
+
+//#region task 3
+// let a = Number(prompt("Enter the number :"));
+// if (a<0) {
+//     alert("musbet eded daxil edin")
+// }
+// let b = Number(prompt("Enter the number :"));
+// if (b>0) {
+//     alert("menfi eded daxil edin")
+// }
+//#endregion
